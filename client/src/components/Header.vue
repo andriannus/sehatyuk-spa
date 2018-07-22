7731bc78d77f4c045138afeaa0760380450a1d66
Andre <andriansimamora@gmail.com>
2018-07-23 01:08:25 +0700

    Memisahkan navigasi ke komponen yang baru

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,42 +1,6 @@
 <template>
 	<div>
-		<nav class="navbar is-fixed-top is-danger" role="navigation" aria-label="main navigation">
-			<div class="navbar-brand">
-				<a class="navbar-item">
-					SEHAT YUK
-				</a>
-
-				<a role="button" class="navbar-burger has-text-white" :class="{ 'is-active': isActive }" aria-label="menu" aria-expanded="false" @click="switchMenu">
-					<span aria-hidden="true"></span>
-					<span aria-hidden="true"></span>
-					<span aria-hidden="true"></span>
-				</a>
-			</div>
-
-			<div class="navbar-menu" :class="{ 'is-active': isActive }">
-				<div class="navbar-start">
-					<div class="navbar-item">
-						<div class="field is-grouped">
-							<p class="control">
-								<a class="button is-dark" href="https://github.com/andriannus/sehatyuk-spa" target="_blank">
-									<span class="icon">
-										<i class="fab fa-github"></i>
-									</span>
-									<span>Github</span>
-								</a>
-							</p>
-						</div>
-					</div>
-				</div>
-
-				<div class="navbar-end">
-					<router-link class="navbar-item" to="/">Beranda</router-link>
-					<router-link class="navbar-item" to="puskesmas">Puskesmas</router-link>
-					<router-link class="navbar-item" to="rsk">Rumah Sakit Khusus</router-link>
-					<router-link class="navbar-item" to="rsu">Rumah Sakit Umum</router-link>
-				</div>
-			</div>
-		</nav>
+		<Navigation />
 
 		<router-view />
 		
@@ -49,20 +13,16 @@
 </template>
 
 <script>
+import Navigation from './Navigation'
+
 export default {
 	name: 'Header',
 	metaInfo: {
 		title: 'Selamat Datang di Sehat Yuk'
 	},
 
-	data: () => ({
-		isActive: false
-	}),
-
-	methods: {
-		switchMenu () {
-			this.isActive = !this.isActive
-		}
+	components: {
+		Navigation
 	}
 }
 </script>
---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,55 @@
+<template>
+	<div>
+		<nav class="navbar is-fixed-top is-success" role="navigation" aria-label="main navigation">
+			<div class="navbar-brand">
+				<a class="navbar-item">
+					SEHAT YUK
+				</a>
+
+				<a role="button" class="navbar-burger has-text-white" :class="{ 'is-active': isActive }" aria-label="menu" aria-expanded="false" @click="switchMenu">
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+					<span aria-hidden="true"></span>
+				</a>
+			</div>
+
+			<div class="navbar-menu" :class="{ 'is-active': isActive }">
+				<div class="navbar-start">
+					<div class="navbar-item">
+						<div class="field is-grouped">
+							<p class="control">
+								<a class="button is-dark" href="https://github.com/andriannus/sehatyuk-spa" target="_blank">
+									<span class="icon">
+										<i class="fab fa-github"></i>
+									</span>
+									<span>Github</span>
+								</a>
+							</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="navbar-end">
+					<router-link class="navbar-item" to="/">Beranda</router-link>
+					<router-link class="navbar-item" to="puskesmas">Puskesmas</router-link>
+					<router-link class="navbar-item" to="rsk">Rumah Sakit Khusus</router-link>
+					<router-link class="navbar-item" to="rsu">Rumah Sakit Umum</router-link>
+				</div>
+			</div>
+		</nav>
+	</div>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		isActive: false
+	}),
+
+	methods: {
+		switchMenu () {
+			this.isActive = !this.isActive
+		}
+	}
+}
+</script>
